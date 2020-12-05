0bed5f6ea3e256eb825f036e1548f27968805e96
Jürgen Minwegen <juergen.minwegen@apoke.org>
2020-12-05 15:03:51 +0100

    added y home pos offset for hero me gen 5

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1348,7 +1348,7 @@
 // Manually set the home position. Leave these undefined for automatic settings.
 // For DELTA this is the top-center of the Cartesian print volume.
 //#define MANUAL_X_HOME_POS 0
-//#define MANUAL_Y_HOME_POS 0
+#define MANUAL_Y_HOME_POS -9  //did this because of hero me gen 5
 //#define MANUAL_Z_HOME_POS 0
 
 // Use "Z Safe Homing" to avoid homing with a Z probe outside the bed area.
